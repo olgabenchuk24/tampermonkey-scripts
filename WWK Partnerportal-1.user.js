f56324f4f813f1db05aa2746a3fc216f685b7461
olgabenchuk24 <123236512+olgabenchuk24@users.noreply.github.com>
2025-07-04 11:52:15 +0200

    Update WWK Partnerportal-1.user.js

--- WWK Partnerportal-1.user.js
+++ WWK Partnerportal-1.user.js
@@ -6,7 +6,7 @@
 // @match        https://portal.wwk.de/group/vip/*
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // ----------------------------------------------------
@@ -32,53 +32,60 @@
 
     async function run() {
         try {
-
             const documentLink = document.querySelector('a[href="/group/vip/dokumentenpostfach"]');
             documentLink.click();
 
             await waitFor(2000);
 
-            const checkbox = document.getElementById('_DokumentenpostfachPortlet_WAR_DokumentenpostfachPortletApp_INSTANCE_Rc9XlwPoylnE_:dokumentenpostfach_form:j_idt19_input');
+            const checkbox = document.getElementById(
+                '_DokumentenpostfachPortlet_WAR_DokumentenpostfachPortletApp_INSTANCE_Rc9XlwPoylnE_:dokumentenpostfach_form:j_idt19_input'
+            );
 
             if (!checkbox) {
-               throw new Error('Failed to find the checkbox.');
+                throw new Error('Failed to find the checkbox.');
             }
 
             if (checkbox.checked) {
-              checkbox.click();
+                checkbox.click();
             }
 
             await waitFor(2000);
 
             let documentToDownload = document.querySelector('table tr:first-of-type td:nth-of-type(3) a');
+            let downloadCount = 0;
 
             if (!documentToDownload) {
-                alert('There is no new documents to download.');
+                alert('Es gibt keine neuen Dokumente zum Herunterladen.');
             }
 
             while (documentToDownload) {
                 await waitFor(1000);
                 documentToDownload.click();
+                downloadCount++;
+
                 await waitFor(3000);
+
                 const newDocumentToDownload = document.querySelector('table tr:first-of-type td:nth-of-type(3) a');
 
-                const isNewDocumentToDownlowedTheSameAsThePreviouslyDownloadedOne = documentToDownload === newDocumentToDownload;
+                const isNewDocumentToDownlowedTheSameAsThePreviouslyDownloadedOne =
+                    documentToDownload === newDocumentToDownload;
+
                 if (isNewDocumentToDownlowedTheSameAsThePreviouslyDownloadedOne) {
-                    throw new Error('The previous document has not yet disappeared after 2s');
+                    throw new Error('The previous document has not yet disappeared');
                 }
 
                 documentToDownload = newDocumentToDownload;
             }
 
-            alert('All new documents have been downloaded.');
+            if (downloadCount > 0) {
+                alert('Alle neuen Dokumente wurden heruntergeladen.');
+            }
         } catch (err) {
-            alert('Automation failed:', err);
+            alert('Automation failed:', err.message);
         }
     }
-
 
     async function waitFor(ms) {
         await new Promise(r => setTimeout(r, ms));
     }
 })();
-
